7baea0fdf0e92c30fe383ab303023833ba9e16cc
NatapasL <natapas.lue@gmail.com>
2024-07-19 23:09:12 +0700

    Change column abbr to default name

--- internal/infrastructure/model/series_name.go
+++ internal/infrastructure/model/series_name.go
@@ -5,11 +5,11 @@ import (
 )
 
 type SeriesName struct {
-	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	SeriesID uuid.UUID `gorm:"type:uuid"`
-	Name     string
-	Language string
-	Abbr     bool
+	ID          uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	SeriesID    uuid.UUID `gorm:"type:uuid"`
+	Name        string
+	Language    string
+	DefaultName bool
 }
 
 func (SeriesName) TableName() string {
