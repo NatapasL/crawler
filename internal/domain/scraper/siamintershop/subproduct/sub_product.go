31cc0e04df99c1917575557728b125ed3cd1e303
NatapasL <natapas.lue@gmail.com>
2024-07-23 02:45:02 +0700

    Transform sub product to product

--- internal/domain/scraper/siamintershop/subproduct/sub_product.go
+++ internal/domain/scraper/siamintershop/subproduct/sub_product.go
@@ -1,5 +1,12 @@
 package subproduct
 
+import (
+	"manga-crawler/internal/domain/scraper/productupdater"
+	"manga-crawler/internal/domain/scraper/siamintershop"
+
+	"github.com/google/uuid"
+)
+
 type SubProduct struct {
 	ProductId    string `json:"product_id"`
 	ProductName  string `json:"product_name"`
@@ -10,4 +17,23 @@ type SubProduct struct {
 	ProductFullPrice float64 `json:"product_full_price"`
 
 	ProductSalePercent float64 `json:"product_sale_percent"`
+
+	ProductFullUrl string `json:"product_full_url"`
+}
+
+func (sp SubProduct) ToProduct() productupdater.Product {
+	wholesaleID, _ := uuid.Parse(siamintershop.SiamintershopID)
+
+	return productupdater.NewProduct(productupdater.NewProductArgs{
+		Name:        sp.ProductName,
+		WholesaleID: wholesaleID,
+		ExternalID:  sp.ProductId,
+		SourceUrl:   sp.ProductFullUrl,
+		Price:       sp.ProductMaxPrice,
+		Discounted:  sp.isDiscounted(),
+	})
+}
+
+func (sp SubProduct) isDiscounted() bool {
+	return sp.ProductMaxPrice != sp.ProductFullPrice
 }
