3cb63b0320586e02cd3539b1a08e8ce0237c0c3e
NatapasL <natapas.lue@gmail.com>
2024-07-22 18:56:36 +0700

    Move file

--- cmd/crawler/container/seriesnamematcher/series_name_matcher_module.go
+++ cmd/crawler/container/seriesnamematcher/series_name_matcher_module.go
@@ -2,7 +2,7 @@ package seriesnamematcher
 
 import (
 	"manga-crawler/cmd/crawler/container"
-	"manga-crawler/internal/domain/scraper/seriesnamematcher"
+	"manga-crawler/internal/domain/seriesnamematcher"
 )
 
 type SeriesNameMatcherModule struct {
