8c9489bb1f230c7e3b0b7b686404a8bac3b5484d
NatapasL <natapas.lue@gmail.com>
2024-07-25 13:05:19 +0700

    Add retry to api request

--- internal/infrastructure/api_gateway/siamintershop/api_request.go
+++ internal/infrastructure/api_gateway/siamintershop/api_request.go
@@ -2,18 +2,24 @@ package siamintershop
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
 type ApiRequest struct{}
 
-func (ApiRequest) Request(request *http.Request) ([]byte, error) {
+func (r ApiRequest) Request(request *http.Request) ([]byte, error) {
 	var emptyByte []byte
 
-	client := &http.Client{}
-	res, err := client.Do(request)
+	res, err := r.doRequest(request)
 	if err != nil {
-		return emptyByte, err
+		log.Printf("%s, retrying...", err.Error())
+
+		res, err = r.retry(request)
+		if err != nil {
+			return emptyByte, err
+		}
 	}
 
 	responseData, err := io.ReadAll(res.Body)
@@ -22,4 +28,26 @@ func (ApiRequest) Request(request *http.Request) ([]byte, error) {
 	}
 
 	return responseData, nil
+}
+
+func (ApiRequest) doRequest(request *http.Request) (*http.Response, error) {
+	client := &http.Client{}
+	return client.Do(request)
+}
+
+func (r ApiRequest) retry(request *http.Request) (*http.Response, error) {
+	var res *http.Response
+	var err error
+
+	retryDelays := [3]time.Duration{1 * time.Second, 3 * time.Second, 9 * time.Second}
+	for _, delay := range retryDelays {
+		time.Sleep(delay)
+
+		res, err = r.doRequest(request)
+		if err == nil {
+			return res, err
+		}
+	}
+
+	return nil, err
 }
