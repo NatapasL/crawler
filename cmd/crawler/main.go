6a9715d02cf061c2239eed9ea94d9f4fad5a0b01
NatapasL <natapas.lue@gmail.com>
2024-07-25 21:51:05 +0700

    Delete unused

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"manga-crawler/cmd/crawler/container"
 	"manga-crawler/cmd/crawler/container/siamintershop"
 	"manga-crawler/config"
+	"manga-crawler/internal/domain/catalogupdate/series"
+	"manga-crawler/internal/domain/lookup/wholesale"
+	"manga-crawler/internal/domain/scraper"
 	"manga-crawler/internal/infrastructure/database/postgres"
 )
 
@@ -15,6 +19,15 @@ func main() {
 		return
 	}
 
-	siamintershopModule := siamintershop.InitializeSiamintershopModule(siamintershop.SiamintershopModuleDependencies{})
-	log.Println(siamintershopModule)
+	repositoryModule := container.InitializeRepositoryModule(container.RepositoryModuleDependencies{DB: db})
+	seriesFinderService := series.NewSeriesFinderService(series.NewSeriesFinderServiceDependencies{
+		SeriesRepository:            repositoryModule.SeriesRepository,
+		SeriesNameMatcherRepository: repositoryModule.SeriesNameMatcherRepository,
+	})
+	siamintershopModule := siamintershop.InitializeSiamintershopModule(siamintershop.SiamintershopModuleDependencies{
+		SeriesFinder: *seriesFinderService,
+	})
+
+	scraperFactory := scraper.NewScraperFactory()
+	scraperFactory.Register(string(wholesale.SiamintershopID), siamintershopModule.Scraper)
 }
